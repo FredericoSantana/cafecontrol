35b510b0fa8c7d5794bcb7b84995095d536c394c
Frederico Santana <fredericosantana@hotmail.com.br>
2021-12-07 23:15:43 -0300

    Navegação por categorias

--- themes/cafeweb/blog-list.php
+++ themes/cafeweb/blog-list.php
@@ -3,7 +3,9 @@
         <img title="<?= $post->title; ?>" alt="<?= $post->title; ?>" src="<?= image($post->cover, 600, 340); ?>"/>
     </a>
     <header>
-        <p class="meta"><?= $post->category()->title; ?> &bull; Por <?= "{$post->author()->first_name} {$post->author()->last_name}" ?> &bull; <?= date_fmt($post->post_at); ?></p>
+        <p class="meta">
+          <a title="Artigos em <?= $post->category()->title; ?>" href="<?= url("/blog/em/{$post->category()->uri}"); ?>"><?= $post->category()->title; ?></a>
+           &bull; Por <?= "{$post->author()->first_name} {$post->author()->last_name}" ?> &bull; <?= date_fmt($post->post_at); ?></p>
         <h2>
           <a title="<?= $post->title; ?>" href="<?= url("/blog/{$post->uri}"); ?>">
             <?= $post->title; ?>
